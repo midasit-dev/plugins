bb6b8d00f61237d6d689ccc94071cf8d033d0fd2
kh1012 <kh1012@midasit.com>
2024-10-10 14:17:06 +0900

    Fixed the issue, Easy Load Combination (v1.1.0)
    - Fixed the issue where Moving Load of Eurocode was not being loaded.

--- projects/easy-load-combinations/src/Workers/MovingLoadWorker.js
+++ projects/easy-load-combinations/src/Workers/MovingLoadWorker.js
@@ -2,60 +2,64 @@ import { loadData, hasError, isDemo } from "../utils";
 import { DBVARIANT } from "./dictionary";
 
 const NationalDefinitions = {
-	TRANS: "TR",
-	EUROCODE: "EU",
-	CHINA: "CH",
-	INDIA: "ID",
-	JAPAN: "JP",
-	POLAND: "PL",
-	BS: "BS",
+  TRANS: "TR",
+  EUROCODE: "EU",
+  CHINA: "CH",
+  INDIA: "ID",
+  JAPAN: "JP",
+  POLAND: "PL",
+  BS: "BS",
 };
 
 export const DataLoader = async () => {
-	const DBNAME = DBVARIANT.MOVING_LOAD;
-	const DBCODE = DBVARIANT.MOVING_LOAD_VARIANT;
+  const DBNAME = DBVARIANT.MOVING_LOAD;
+  const DBCODE = DBVARIANT.MOVING_LOAD_VARIANT;
 
-	const natlCodeData = isDemo()
-		? {
-				MVCD: {
-					1: {
-						CODE: "EUROCODE",
-					},
-				},
-		  }
-		: await loadData(DBVARIANT.PATH + DBCODE);
-	if (hasError(natlCodeData)) return [];
+  const natlCodeData = isDemo()
+    ? {
+        MVCD: {
+          1: {
+            CODE: "EUROCODE",
+          },
+        },
+      }
+    : await loadData(DBVARIANT.PATH + DBCODE);
+  if (hasError(natlCodeData)) return [];
 
-	const natlCode = natlCodeData[DBCODE][1]["CODE"];
+  const natlCode = natlCodeData[DBCODE][1]["CODE"];
 
-	let natlCodePostFix = "";
-	if (NationalDefinitions[natlCode] !== undefined)
-		natlCodePostFix = NationalDefinitions[natlCode];
+  let natlCodePostFix = "";
+  if (NationalDefinitions[natlCode] !== undefined)
+    natlCodePostFix = NationalDefinitions[natlCode];
 
-	const rawData = isDemo()
-		? {
-				MVLD: {
-					1: {
-						LCNAME: "Moving Load 1",
-					},
-					2: {
-						LCNAME: "Moving Load 2",
-					},
-					3: {
-						LCNAME: "Moving Load 3",
-					},
-				},
-		  }
-		: await loadData(DBVARIANT.PATH + DBNAME + natlCodePostFix);
-	if (hasError(rawData)) return [];
-	if (rawData[DBNAME] === undefined) return [];
+  //국가에 따라 PostFix가 있을 수 있다. (KOREA는 없음)
+  const ROOTNAME = DBNAME + natlCodePostFix;
 
-	let registeredNames = [];
-	const dbData = rawData[DBNAME];
-	for (const value in dbData) {
-		const targetData = dbData[value];
-		registeredNames.push(targetData.LCNAME);
-	}
+  const rawData = isDemo()
+    ? {
+        MVLD: {
+          1: {
+            LCNAME: "Moving Load 1",
+          },
+          2: {
+            LCNAME: "Moving Load 2",
+          },
+          3: {
+            LCNAME: "Moving Load 3",
+          },
+        },
+      }
+    : await loadData(DBVARIANT.PATH + ROOTNAME);
 
-	return registeredNames;
+  if (hasError(rawData)) return [];
+  if (rawData[ROOTNAME] === undefined) return [];
+
+  let registeredNames = [];
+  const dbData = rawData[ROOTNAME];
+  for (const value in dbData) {
+    const targetData = dbData[value];
+    registeredNames.push(targetData.LCNAME);
+  }
+
+  return registeredNames;
 };
